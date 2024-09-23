623802672d4aa440ec96d8f930d3f73a7ecebc7b
IsrarAliMCSD <i4israr_6@yahoo.com>
2024-09-23 19:58:57 +0500

    Add files via upload

---
+++ Console_RabbitMQApp1/Console_RabbitMQApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Console_RabbitMQApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                // Declare a queue
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+                string message = "Hello RabbitMQ!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                // Publish a message
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                int i = 0;
+                while (i < 10)
+                {
+                    message = "Hello RabbitMQ!" + i;
+                    body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange: "",
+                                   routingKey: "hello",
+                                   basicProperties: null,
+                                   body: body);
+                    Thread.Sleep(10000);
+                    i++;
+                }
+                Console.WriteLine($" [x] Sent {message}");
+            }
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
